7423b2a1c70b53bc8e13c641a27a26e916113c40
kevin_pc <254337598@qq.com>
2023-09-03 20:40:36 +0800

    add clac simple demo

---
+++ import_shard_lib/calc/calc.cpp
@@ -0,0 +1,14 @@
+#include "calc.h"
+
+void add_func(int a ,int b )
+{
+    int result=a+b;
+    std::cout<<result<<std::endl;
+    return;
+}
+
+void reduce_func(int a ,int b)
+{
+    int result =a-b;
+    std::cout<<result<<std::endl;
+}
---
+++ import_shard_lib/calc/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "calc.h"
+
+int main()
+{
+    add_func(3,4);
+    reduce_func(10,4);
+    return 0;
+}
